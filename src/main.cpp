b4bc88fbc720e130dada7d3ec5554ff7699f9fab
Conan Bitter <conan.bitter@gmail.com>
2023-11-18 20:23:42 +0300

    Add GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello" << std::endl;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
