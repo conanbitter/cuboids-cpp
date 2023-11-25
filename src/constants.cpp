4b252052788dc2accc276e364b5d627e782b0896
Conan Bitter <conan.bitter@gmail.com>
2023-11-25 19:56:55 +0300

    Add constants

--- src/constants.cpp
+++ src/constants.cpp
@@ -5,3 +5,28 @@ Shape SHAPE_SHIP = Shape(true, {
                                    Vector2D(0.7, -0.8),
                                    Vector2D(-0.7, -0.8),
                                });
+
+Shape SHAPE_ASTER = Shape(true, {
+                                    Vector2D(-1.0, -1.0),
+                                    Vector2D(-1.0, 1.0),
+                                    Vector2D(1.0, 1.0),
+                                    Vector2D(1.0, -1.0),
+                                });
+
+Shape SHAPE_PROJ = Shape(true, {
+                                   Vector2D(0.0, 0.8),
+                                   Vector2D(0.2, 0.0),
+                                   Vector2D(0.0, -1.2),
+                                   Vector2D(-0.2, 0.0),
+                               });
+
+const float SHIP_SCALE = 0.1;
+const float SHIP_MAX_SPEED = 0.1;
+const float SHIP_ACCELERATION = 0.001;
+const float SHIP_DRAG = 0.0001;
+const float SHIP_ROT_SPEED = 0.05;
+
+const float ASTER_SCALE = 0.2;
+
+const float PROJ_SCALE = 0.05;
+const float PROJ_SPEED = 0.01;
--- src/constants.hpp
+++ src/constants.hpp
@@ -3,3 +3,18 @@
 #include "geometry.hpp"
 
 extern Shape SHAPE_SHIP;
+
+extern Shape SHAPE_ASTER;
+
+extern Shape SHAPE_PROJ;
+
+const float SHIP_SCALE;
+const float SHIP_MAX_SPEED;
+const float SHIP_ACCELERATION;
+const float SHIP_DRAG;
+const float SHIP_ROT_SPEED;
+
+const float ASTER_SCALE;
+
+const float PROJ_SCALE;
+const float PROJ_SPEED;
